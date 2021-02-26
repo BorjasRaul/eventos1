869e17cdb2371e00fadc9349e304ff89193d34d0
Angel Acosta <162t0331@itsm.edu.mx>
2021-02-26 00:12:51 -0600

    "adelanto"

--- app/src/main/java/com/example/eventos/MainActivity.kt
+++ app/src/main/java/com/example/eventos/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.eventos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       recyclerView = findViewById(R.id.recyclerView)
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerViewAdapter ()
+        recyclerView.adapter = adapter
+
     }
 }
